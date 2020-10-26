b557a411ef9c531d159bab6053b25d36c606c141
s-sutton3 <63067467+s-sutton3@users.noreply.github.com>
2020-10-26 13:14:58 -0600

    Update
    Added a value for "num5" and removed the +s at the start of document.GetElementById on line 8-12.

--- NumericCalc2.js
+++ NumericCalc2.js
@@ -5,11 +5,11 @@ document.getElementById("calculate").addEventListener("click", calcInterest);
 function calcInterest() {
 
 // Input
-let num1 =+document.getElementById("num1");
-let num2 =+document.getElementById("num2");
-let num3 =+document.getElementById("num3");
-let num4 =+document.getElementById("num4");
-let num5 =+document.getElementById("num5");
+let num1 = document.getElementById("num1").value;
+let num2 = document.getElementById("num2").value;
+let num3 = document.getElementById("num3").value;
+let num4 = document.getElementById("num4").value;
+let num5 = document.getElementById("num5").value;
 
 // Process
 let interest = (num1 * (1 + num2 / num3) ** (num4 / num5))
