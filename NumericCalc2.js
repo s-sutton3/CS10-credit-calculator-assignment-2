bf2dbe2235ad55aa19a0342bad062af76eae042f
s-sutton3 <63067467+s-sutton3@users.noreply.github.com>
2020-10-26 13:01:54 -0600

    Update:
    Removed space before the document.getElementById on lines 8-12

--- NumericCalc2.js
+++ NumericCalc2.js
@@ -5,11 +5,11 @@ document.getElementById("calculate").addEventListener("click", calcInterest);
 function calcInterest() {
 
 // Input
-let num1 = +document.getElementById("num1");
-let num2 = +document.getElementById("num2");
-let num3 = +document.getElementById("num3");
-let num4 = +document.getElementById("num4");
-let num5 = +document.getElementById("num5");
+let num1 =+document.getElementById("num1");
+let num2 =+document.getElementById("num2");
+let num3 =+document.getElementById("num3");
+let num4 =+document.getElementById("num4");
+let num5 =+document.getElementById("num5");
 
 // Process
 let interest = (num1 * (1 + num2 / num3) ** (num4 / num5))
